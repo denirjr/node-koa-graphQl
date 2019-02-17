f8af604cc4a484c23c6c80ed8e7e43fca922c701
DenirJr <denirdamasceno7@gmail.com>
2019-02-17 14:23:17 -0300

    update and delete mutation

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -1,10 +1,11 @@
 const { GraphQLObjectType, GraphQLString } = require('graphql');
-const gadgetGraphQLType =  require('./gadgetType');
+const gadgetGraphQLType = require('./gadgetType');
 const Gadget = require('../models/gadgets');
 
 const Mutation = new GraphQLObjectType({
     name: 'mutation',
     fields: {
+
         addGadget: {
             type: gadgetGraphQLType,
             args: {
@@ -21,6 +22,43 @@ const Mutation = new GraphQLObjectType({
                     price: args.price,
                 })
                 return newGadget.save();
+            }
+        },
+
+        updateGadget: {
+            type: gadgetGraphQLType,
+            args: {
+                id: { type: GraphQLString },
+                name: { type: GraphQLString },
+                release_date: { type: GraphQLString },
+                by_company: { type: GraphQLString },
+                price: { type: GraphQLString }
+            },
+            resolve(parent, args) {
+                return Gadget.findById(args.id)
+                    .then(gadget => {
+                        gadget.name = args.name
+                        gadget.release_date = args.release_date,
+                            gadget.by_company = args.by_company,
+                            gadget.price = args.price
+
+                        return gadget.save();
+                    })
+                    .then(updateGadget => updateGadget)
+                    .catch(err => console.log(err))
+            }
+        },
+
+        removeGadget: {
+            type: gadgetGraphQLType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parent, args) {
+                return Gadget.findOneAndDelete(args.id).exec()
+                .then(gadget => gadget.remove())
+                .then(deleteGadget => deleteGadget)
+                .catch(err => console.log(err))
             }
         }
     }
